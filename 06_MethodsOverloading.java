217b638249d3481b62db6439f13e6a39b419bc80
Abdulsamed SAY <32686927+samedsay@users.noreply.github.com>
2020-08-29 16:05:47 +0200

    Update 06_MethodsOverloading.java

--- 06_MethodsOverloading.java
+++ 06_MethodsOverloading.java
@@ -1,26 +1,51 @@
-//package com.samedify.methodsOverloading;
+package com.samedify.methodsOverloading;
 
 public class Main {
     public static void main(String[] args) {
 
-        calculatePoints("Samed", 100);
-        calculatePoints(200);
-        calculatePoints();
+        int newScore = calculateScore("Samed", 100);
+        System.out.println(newScore);
+
+        newScore = calculateScore("Jane");
+        System.out.println(newScore);
+
+        newScore = calculateScore(200);
+        System.out.println(newScore);
+
+        newScore = calculateScore();
+        System.out.println(newScore);
+
+        double newScore2 = calculateScore("Samed", 1000, 300);
+        System.out.println(newScore2);
+
+
     }
 
-    public static int calculatePoints(String playerName, int points) {
-        System.out.println("Player Name: " + playerName + "\nPlayer Points: " + points);
-        return points * 10;
+    public static int calculateScore(String name, int score) {
+        System.out.println("Player Name: " + name + "\nPlayer Score: " + score);
+        return score * 10;
     }
 
-    public static int calculatePoints(int points) {
-        System.out.println("Player Name: Unknown" + "\nPlayer Points: " + points);
-        return points * 10;
+    public static int calculateScore(String name) {
+        int score = 0;
+        System.out.println("Player Name: " + name + "\nPlayer Score: Unknown");
+        return score * 10;
     }
 
-    public static int calculatePoints() {
-        int points = 0;
-        System.out.println("Player Name: Unknown " + "\nPlayer Points: " + points);
-        return points;
+    public static int calculateScore(int score) {
+        System.out.println("Player Name: Unknown" + "\nPlayer Score: " + score);
+        return score * 10;
     }
+
+    public static int calculateScore() {
+        System.out.println("Player Name: Unknown" + "\nPlayer Score: Unknown");
+        return 0;
+    }
+
+    public static double calculateScore(String a, int b, int c) {
+        System.out.println("Player Name: " + a + "\nPlayer Score: " + (b + c));
+        return b + c;
+    }
+
+
 }
